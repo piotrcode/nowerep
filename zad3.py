127ddaa0f1ecbd894827602f660a9bbdc2ef9cb3
NietzscheanskiYerbacz <45830185+NietzscheanskiYerbacz@users.noreply.github.com>
2020-12-22 13:14:10 +0100

    Update zad3.py

---
+++ zad3.py
@@ -0,0 +1,11 @@
+m = int(input('podaj pierwsza liczbe'))
+n = int(input('podaj druga liczbe'))
+def nwd(a,b):
+    while a != b:
+        if a > b:
+            a = a - b
+        else:
+            b = b - a
+    return a
+
+print(m*n//nwd(m, n))
